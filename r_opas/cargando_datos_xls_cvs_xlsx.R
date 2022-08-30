713287b5734ec99d94ab0acf69ffe9a22b6d689b
iran_mx2 <iranmolinadev@gmail.com>
2022-08-30 17:03:37 -0600

    cambios de gitignore

---
+++ r_opas/cargando_datos_xls_cvs_xlsx.R
@@ -0,0 +1,9 @@
+datos_sg <- rio::import("datos\\sg_2022.csv")
+datos_pop <- rio::import("datos\\pop_20_21.xls")
+datos_srag <- rio::import("datos\\srag_20_22.xlsx")
+
+rm(datos_sg)
+
+View(datos_sg)
+
+warnings()
